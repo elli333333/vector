5d03fb06139e4c91a666ce00066eab8f968bd0a6
Ellie Macdonald <stuartmacdonald201@gmail.com>
2022-03-21 19:28:07 +0000

    segfault fixed, ansi 16 color table added

---
+++ ansi_colors.h
@@ -0,0 +1,25 @@
+//
+// Created by ellie on 21/03/2022.
+//
+
+#ifndef VECTOR_ANSI_COLORS_H
+#define VECTOR_ANSI_COLORS_H
+
+#define BLACK               0x000000
+#define RED                 0xDE382B
+#define GREEN               0x39B54A
+#define YELLOW              0xFFC706
+#define BLUE                0x006FB8
+#define MAGENTA             0x762671
+#define CYAN                0x30B5B8
+#define WHITE               0xCCCCCC
+#define BRIGHT_BLACK        0x000000
+#define BRIGHT_RED          0xFF0000
+#define BRIGHT_GREEN        0x00FF00
+#define BRIGHT_YELLOW       0xFFFF00
+#define BRIGHT_BLUE         0x0000FF
+#define BRIGHT_MAGENTA      0xFF00FF
+#define BRIGHT_CYAN         0x00FFFF
+#define BRIGHT_WHITE        0xFFFFFF
+
+#endif //VECTOR_ANSI_COLORS_H
--- fbtest.c
+++ fbtest.c
@@ -9,16 +9,18 @@
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 
+#include "ansi_colors.h"
+
 uint32_t pixel_color(uint8_t r, uint8_t g, uint8_t b, struct fb_var_screeninfo* vinfo) {
     return (r<<vinfo->red.offset) | (g<<vinfo->green.offset) | (b<<vinfo->blue.offset);
 }
 
 void test_pattern(struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo,  uint8_t *fbp) {
     for (int x = 0; x < vinfo->xres; x++) {
-        for (int y = 0; x < vinfo->yres; y++) {
+        for (int y = 0; y < vinfo->yres; y++) {
             long location =
                     (x + vinfo->xoffset) * (vinfo->bits_per_pixel / 8) + (y + vinfo->yoffset) * finfo->line_length;
-            *((uint32_t*)(fbp + location)) = pixel_color(0xff, 0xff, 0xff, vinfo);
+            *((uint32_t*)(fbp + location)) = pixel_color(0xff, 0x00, 0xff, vinfo);
 
         }
     }
@@ -43,47 +45,7 @@ int main() {
 
     uint8_t *fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0);
 
-//    test_pattern(&vinfo, &finfo, fbp);
+    test_pattern(&vinfo, &finfo, fbp);
 
-    int x,y,i,j;
-    for (j = 0; j < 7; ++j) {
-        for (i = 0; i < 256; ++i) {
-
-            for (x = 0; x < vinfo.xres; x++) {
-                for (y = 0; y < vinfo.yres; y++) {
-                    long location =
-                            (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) + (y + vinfo.yoffset) * finfo.line_length;
-                    unsigned char r = 0, g = 0, b = 0;
-                    switch (j) {
-                        case 0:
-                            r = i, g = i, b = i;
-                            break;
-                        case 1:
-                            r = i;
-                            break;
-                        case 2:
-                            g = i;
-                            break;
-                        case 3:
-                            b = i;
-                            break;
-                        case 4:
-                            r = i, g = i;
-                            break;
-                        case 5:
-                            r = i, b = i;
-                            break;
-                        case 6:
-                            g = i, b = i;
-                            break;
-                        default:
-                            r = i, g = i, b = i;
-                            break;
-                    }
-                    *((uint32_t *) (fbp + location)) = pixel_color(r, g, b, &vinfo);
-                }
-            }
-        }
-    }
     return 0;
 }
