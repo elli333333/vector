15fc7661fb866096f4686de97c61613d5f67fc21
Ellie Macdonald <stuartmacdonald201@gmail.com>
2022-03-24 18:22:08 +0000

    macros! MACROS WERE THE SOLUTION

--- fbtest.c
+++ fbtest.c
@@ -13,17 +13,14 @@
 
 #include "pallet_ansi.h"
 
-uint8_t * buf_a_ptr;
-uint8_t * buf_b_ptr;
+#define pixel(x, y) *((uint32_t*)(buf_b_ptr + (x+var_info.xoffset) * (var_info.bits_per_pixel/8) + (y+var_info.yoffset) * fix_info.line_length))
+
+void * buf_a_ptr;
+void * buf_b_ptr;
 struct fb_var_screeninfo var_info;
 struct fb_fix_screeninfo fix_info;
 int fb_dev;
 
-//uint32_t * pixmap(int x, int y) {
-//    return ((uint32_t *)(buf_b_ptr + ((x+var_info.xoffset) * (var_info.bits_per_pixel/8) + (y+var_info.yoffset) * fix_info.line_length)));
-//}
-
-//uint32_t (buf_b_ptr)[fix_info.line_length / 4] = (uint32_t (*)[fix_info.line_length / 4]) pixmap;
 
 uint32_t set_pixel_color(uint32_t color) {
     uint8_t r,g,b;
@@ -59,8 +56,7 @@ void swap_buffers() {
 void fill_src(uint32_t colour) {
     for (int x = 0; x < var_info.xres; ++x) {
         for (int y = 0; y < var_info.yres; ++y) {
-            long location = (x+var_info.xoffset) * (var_info.bits_per_pixel/8) + (y+var_info.yoffset) * fix_info.line_length;
-            *((uint32_t*)(buf_b_ptr + location)) = set_pixel_color(colour);
+            pixel(x, y) = set_pixel_color(colour);
         }
     }
 }
