52be852549e460aa7899a38cd51ea3fa2d9e2125
Ellie Macdonald <stuartmacdonald201@gmail.com>
2022-03-26 19:01:54 +0000

    rename primitives operating on actual framebuffer, add relative framebuffer

--- fbtest.c
+++ fbtest.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <linux/fb.h>
+#include <linux/kd.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 
@@ -22,6 +23,7 @@ struct fb_var_screeninfo var_info;
 struct fb_fix_screeninfo fix_info;
 int fb_dev;
 
+float rel_fb[240][240];
 
 uint32_t set_pixel_color(uint32_t color) {
     uint8_t r,g,b;
@@ -46,6 +48,9 @@ void init_fb() {
 
     buf_a_ptr = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_dev, (off_t)0);
     buf_b_ptr = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, (off_t)0);
+
+    int tty_dev = open("/dev/tty1", O_RDWR);
+    ioctl(tty_dev, KDSETMODE, KD_GRAPHICS);
 }
 
 void swap_buffers() {
@@ -71,7 +76,7 @@ inline static void set_pixel(int rx, int ry, uint32_t colour) {
     *((uint32_t*)(buf_b_ptr + loc)) = colour;
 }
 
-void line(int x1, int y1, int x2, int y2, uint32_t colour) {
+void r_line(int x1, int y1, int x2, int y2, uint32_t colour) {
     int dx, dy, sdx, sdy, dx_abs, dy_abs, x, y, px, py;
 
     dx = x2 - x1;
@@ -113,36 +118,36 @@ void line(int x1, int y1, int x2, int y2, uint32_t colour) {
     }
 }
 
-void polygon(int no_vertices, int* vertices, uint32_t colour) {
+void r_polygon(int no_vertices, int* vertices, uint32_t colour) {
     for(int i=0;i<no_vertices-1;i++)
     {
-        line(vertices[(i<<1)+0],
-             vertices[(i<<1)+1],
-             vertices[(i<<1)+2],
-             vertices[(i<<1)+3],
-             colour);
+        r_line(vertices[(i << 1) + 0],
+               vertices[(i << 1) + 1],
+               vertices[(i << 1) + 2],
+               vertices[(i << 1) + 3],
+               colour);
     }
-    line(vertices[0],
-         vertices[1],
-         vertices[(no_vertices<<1)-2],
-         vertices[(no_vertices<<1)-1],
-         colour);
+    r_line(vertices[0],
+           vertices[1],
+           vertices[(no_vertices << 1) - 2],
+           vertices[(no_vertices << 1) - 1],
+           colour);
 }
 
-void rectangle_fill(int x1, int y1, int x2, int y2, uint32_t colour) {
+void r_rectangle_fill(int x1, int y1, int x2, int y2, uint32_t colour) {
     for (int y = y1; y < y2; ++y) {
-        line(x1, y, x2, y, colour);
+        r_line(x1, y, x2, y, colour);
     }
 }
 
-void rectangle(int x1, int y1, int x2, int y2, uint32_t colour) {
-    line(x1, y1, x1, y2, colour);
-    line(x1, y1, x2, y1, colour);
-    line(x1, y2, x2, y2, colour);
-    line(x2, y2, x2, y1, colour);
+void r_rectangle(int x1, int y1, int x2, int y2, uint32_t colour) {
+    r_line(x1, y1, x1, y2, colour);
+    r_line(x1, y1, x2, y1, colour);
+    r_line(x1, y2, x2, y2, colour);
+    r_line(x2, y2, x2, y1, colour);
 }
 
-void circle(int cx, int cy, int radius, uint32_t colour) {
+void r_circle(int cx, int cy, int radius, uint32_t colour) {
 
 }
 
@@ -153,7 +158,8 @@ int main() {
     swap_buffers();
 
 
-    rectangle(100, 100, 500, 700, BRIGHT_WHITE);
+    r_rectangle(100, 100, 500, 700, BRIGHT_WHITE);
+    r_rectangle_fill(101, 101, 499, 699, BLUE);
     swap_buffers();
 
     return 0;
