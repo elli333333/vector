8832485995601ad2353d13b45e5661703f2643b6
Ellie Macdonald <stuartmacdonald201@gmail.com>
2023-09-07 10:23:20 +0100

    create entity class

---
+++ src/entity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ellie on 9/7/23.
+//
+
+#include "entity.hpp"
---
+++ src/entity.hpp
@@ -0,0 +1,14 @@
+//
+// Created by ellie on 9/7/23.
+//
+
+#ifndef VECTOR_ENTITY_HPP
+#define VECTOR_ENTITY_HPP
+
+
+class entity {
+
+};
+
+
+#endif //VECTOR_ENTITY_HPP
